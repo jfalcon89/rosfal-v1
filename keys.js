51956c192f73d2fb9c7b00fc84923cd8b7015583
jfalcon89 <jfalcon89@hotmail.com>
2022-08-29 15:29:27 -0400

    deploy a heroku

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: `localhost`,
-        user: `root`,
-        password: `123456`,
-        database: `rosfaldb`
+        host: `us-cdbr-east-06.cleardb.net`,
+        user: `bd0067019c8113`,
+        password: `dd273678`,
+        database: `heroku_e796b3abfae260e`
     }
 }
