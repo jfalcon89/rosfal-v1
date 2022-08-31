82d5a96fdf1868d0019a9f5e3403341276b7ec14
jfalcon89 <jfalcon89@hotmail.com>
2022-08-31 17:19:58 -0400

    se agregan historias de nuestros clientes

---
+++ public/js/scriptSlide.js
@@ -0,0 +1,21 @@
+const imgArray = [
+
+    '/img/historia-2.png',
+    '/img/historia-3.png'
+];
+
+const img = document.querySelector('#slide')
+
+
+let i = 0
+
+const slideShow = () => {
+    img.src = imgArray[i]
+    i = (i < imgArray.length - 1) ? i + 1 : 0
+}
+
+const interval = () => {
+    setInterval(slideShow, 5000)
+}
+
+interval();
