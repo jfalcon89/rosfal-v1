7ea386ec1b6c1a460e8a7e9e2eb607a755d1b8c8
Jose Miguel Falcon <jfalcon89@hotmail.com>
2022-10-03 15:18:32 -0400

    adecuacion codigo database.js se elimina linea de codigo

--- database.js
+++ database.js
@@ -10,16 +10,19 @@ pool.getConnection((err, connection) => {
             console.error("LA CONEXION DE LA BASE DE DATOS FUE CERRADA")
         }
         if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has to many connections');
+            console.error('La base de datos tiene demasiadas conexiones');
         }
         if (err.code == "ECONNREFUSED") {
             console.error("LA CONEXION DE LA BASE DE DATOS FUE RECHADADA")
         }
     }
     if (connection) connection.release();
-    console.log("DB rosfal esta conectada a MySQL local ");
-    return;
+    console.log("DB rosfal esta conectada a MySQL");
+   
+    
+    
 });
+
 
 // CONVITIENDO A PROMESA LAS CONSULTAS DE SQL - ANTERIORMENTE ERA UN CALLBACK 
 pool.query = promisify(pool.query);
