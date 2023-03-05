dd04ffa1d222ddb21d968839a61da41b6faaceb1
Jose Miguel Falcon <jfalcon89@hotmail.com>
2023-03-05 02:44:57 -0400

    se agrega notificacion de solicitud a correo jfalcon

--- routes/solicita-ya.js
+++ routes/solicita-ya.js
@@ -85,6 +85,70 @@ router.post("/solicita-ya", async(req, res) => {
 
         enviarCorreo()
     };
+    // FUNCION QUE ENVIA AL CORREO NOTIFICACION DE SOLICITUD DE PRESTAMOS A ROSFAL
+
+    async function notificacionCorreo() {
+        try {
+            const from = "contacto@rosfal.com"
+            const toNotificacion = "jfalcon@rosfal.com"
+
+            // console.log(nombre + " en enviar correo");
+            // console.log(apellido + " en enviar correo");
+
+            // Configurar la conexión SMTP con el servidor de correo personalizado
+            let transporter = nodemailer.createTransport({
+                host: "mail.privateemail.com",
+                port: 465, // El puerto puede variar según la configuración de su servidor
+                secure: true, // Si utiliza SSL/TLS, establezca este valor en true
+                tls: {
+                    rejectUnauthorized: false
+                },
+                auth: {
+                    user: process.env.USERCORREO,
+                    pass: process.env.PASSCORREO,
+                },
+            });
+
+            // Configurar los detalles del correo electrónico
+            let info = await transporter.sendMail({
+                from: `${from} ROSFAL SOLUCIONES DE PRESTAMOS`,
+                to: `${toNotificacion}`,
+                subject: `Nueva solicitud de prestamo cliente ${nombre} ${apellido}`,
+                html: `
+                
+                <P><strong>Asunto</strong>: Confirmación de solicitud de préstamo</p><br>
+
+                <P><strong>Cliente</strong>: ${nombre} ${apellido}</p>
+            
+                <P><strong>Cedula</strong>: ${cedula}</p>
+
+                <P><strong>Monto solicitado</strong>: ${montoSolicitado}</p>
+
+                <P><strong>Direccion</strong>: ${direccion}</p>
+
+                <P><strong>Email</strong>: ${email}</p>
+
+                <P><strong>Celular</strong>: ${celular}</p><br>
+            
+                <P><strong>Atentamente,</p><br>
+            
+                <h4 style="color: #2D8DBD;">ROSFAL SOLUCIONES DE PRESTAMOS</h4>
+                <P><strong>T.</strong> 829-856-0203 <strong>EMAIL.</strong> contacto@rosfal.com </P>
+                <P>Síguenos en <strong>FB:</strong> Rosfalrd <strong>IG:</strong> @Rosfalrd </P>
+                <a href="www.rosfal.com">www.rosfal.com</a>
+                `
+
+            });
+
+            console.log("Correo enviado: %s", info.messageId);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    notificacionCorreo()
+
     // ----------------------------------
 
 
