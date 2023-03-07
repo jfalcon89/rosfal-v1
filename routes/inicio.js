640cc80a70e0ab1cde72f45a31ebe1e3e6246159
Jose Miguel Falcon <jfalcon89@hotmail.com>
2023-03-06 22:50:12 -0400

    se agrega contador de visitas y registro de origen cliente

--- routes/inicio.js
+++ routes/inicio.js
@@ -5,44 +5,46 @@ const moment = require("moment");
 const pool = require("../database");
 const fetch = require("node-fetch");
 
-const ipapi = require('ipapi.co');
-
 const useragent = require('express-useragent');
 
 router.get('/', async(req, res) => {
-    // console.log(res)
-    // console.log(res.hasHeader)
 
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    // const ip = "8.8.8.8";
+    // const ip = "152.0.12.42";
     const device = req.useragent.isMobile ? 'Mobile' : 'Desktop';
     const browser = req.useragent.browser;
 
-    console.log(ip)
-    console.log(device)
-    console.log(browser)
-
-
-    // try {
-    //     const response = await fetch(`https://ipapi.co/${ip}/json`);
-    //     const data = await response.json();
-    //     const { latitude, longitude, country_name, city, postal } = data;
-
-    //     console.log(data)
-
-    //     console.log(`Latitude: ${latitude}, Longitude: ${longitude}, Country: ${country_name}, City: ${city}, Postal: ${postal}`);
-    // } catch (error) {
-    //     console.error(error);
-    //     res.status(500).send('Error al obtener la ubicación');
-    // }
-
-    fetch('https://ipapi.co/8.8.8.8/json')
+    fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=c48b62c0f8844a7c86cb0020ff90e0d3&ip_address=${ip}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+
+            const pais = data.country;
+            const ciudad = data.city;
+            const latitud = data.latitude;
+            const longitud = data.longitude;
 
 
+            const nuevaVisita = {
+                pais,
+                ciudad,
+                ip,
+                device,
+                browser,
+                latitud,
+                longitud
 
 
+            };
+
+            console.log(nuevaVisita)
+
+            pool.query('INSERT INTO visitas set ?', [nuevaVisita]);
+        })
+
+
+    // console.log(ip)
+    // console.log(device)
+    // console.log(browser)
 
     const arrayNuevoTestimonioDB = await pool.query('SELECT * FROM testimonios WHERE estadoTestimonio="Activo" ');
 
--- routes/solicita-ya.js
+++ routes/solicita-ya.js
@@ -3,8 +3,10 @@ const express = require("express");
 const router = express.Router();
 const moment = require("moment");
 const pool = require("../database");
-
+const fetch = require("node-fetch");
 const nodemailer = require("nodemailer");
+
+const useragent = require('express-useragent');
 
 // no se esta usando
 // const LocalStorage = require('node-localstorage').LocalStorage;
@@ -22,9 +24,86 @@ router.get('/solicita-ya', (req, res) => {
 router.post("/solicita-ya", async(req, res) => {
     const { cedula, nombre, apellido, sexo, estadoCivil, direccion, direccionNegocio, tiempoNegocio, email, telefono, celular, nacionadlidad, nombreFamilia, direccionFamilia, parentescoFamilia, telefonoFamilia, apodoFamilia, empresa, salario, puesto, dirEmpresa, telefonoEmpresa, departamento, tiempoEmpresa, nombreRefPers1, nombreRefPers2, telefonoRefPer1, telefonoRefPer2, tipoPrestamo, banco, numeroCuenta, montoSolicitado, frecuenciaPagos, cantidadPagosSemanales, cantidadPagosDiarios, cantidadPagosQuincenales, cantidadPagosMensuales } = req.body;
 
-    // ----------------------------------
+    // OBTENCION DE PARAMAMETROS CLIENTE
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    // const ip = "152.0.12.42";
+    const device = req.useragent.isMobile ? 'Mobile' : 'Desktop';
+    const browser = req.useragent.browser;
 
-    // FUNCION QUE ENVIA AL CORREO NOTIFICACION DE SOLICITUD DE PRESTAMOS
+    fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=c48b62c0f8844a7c86cb0020ff90e0d3&ip_address=${ip}`)
+        .then(response => response.json())
+        .then(data => {
+
+            // FUNCION QUE ENVIA AL CORREO NOTIFICACION DE SOLICITUD DE PRESTAMOS A ROSFAL
+
+            async function notificacionCorreo() {
+                try {
+                    const from = "contacto@rosfal.com"
+                    const toNotificacion = "jfalcon@rosfal.com"
+
+                    // console.log(nombre + " en enviar correo");
+                    // console.log(apellido + " en enviar correo");
+
+                    // Configurar la conexión SMTP con el servidor de correo personalizado
+                    let transporter = nodemailer.createTransport({
+                        host: "mail.privateemail.com",
+                        port: 465, // El puerto puede variar según la configuración de su servidor
+                        secure: true, // Si utiliza SSL/TLS, establezca este valor en true
+                        tls: {
+                            rejectUnauthorized: false
+                        },
+                        auth: {
+                            user: process.env.USERCORREO,
+                            pass: process.env.PASSCORREO,
+                        },
+                    });
+
+                    // Configurar los detalles del correo electrónico
+                    let info = await transporter.sendMail({
+                        from: `${from} ROSFAL SOLUCIONES DE PRESTAMOS`,
+                        to: `${toNotificacion}`,
+                        subject: `Nueva solicitud de prestamo cliente ${nombre} ${apellido}`,
+                        html: `
+            
+            <P><strong>Asunto</strong>: Confirmación de solicitud de préstamo</p><br>
+
+            <P><strong>Cliente</strong>: ${nombre} ${apellido}</p>
+            <P><strong>Cedula</strong>: ${cedula}</p>
+            <P><strong>Monto solicitado</strong>: ${montoSolicitado}</p>
+            <P><strong>Direccion</strong>: ${direccion}</p>
+            <P><strong>Email</strong>: ${email}</p>
+            <P><strong>Celular</strong>: ${celular}</p>
+            <P><strong>Pais</strong>: ${data.country}</p>
+            <P><strong>Ciudad</strong>: ${data.city}</p>
+            <P><strong>Direccion ip</strong>: ${ip}</p>
+            <P><strong>Dispositivo</strong>: ${device}</p>
+            <P><strong>Navegador</strong>: ${browser}</p>
+            <P><strong>Latitud</strong>: ${data.latitude} <strong>longitud</strong>: ${data.longitude}</p><br>
+        
+            <P>Atentamente,</p>
+        
+            <h4 style="color: #2D8DBD;">ROSFAL SOLUCIONES DE PRESTAMOS</h4>
+            <P><strong>T.</strong> 829-856-0203 <strong>EMAIL.</strong> contacto@rosfal.com </P>
+            <P>Síguenos en <strong>FB:</strong> Rosfalrd <strong>IG:</strong> @Rosfalrd </P>
+            <a href="www.rosfal.com">www.rosfal.com</a>
+            `
+
+                    });
+
+                    console.log("Correo enviado: %s", info.messageId);
+
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
+            notificacionCorreo()
+
+
+        })
+
+
+    // FUNCION QUE ENVIA NOTIFICACION DE SOLICITUD DE PRESTAMOS AL CLIENTE
     if (email) {
         async function enviarCorreo() {
             try {
@@ -85,69 +164,7 @@ router.post("/solicita-ya", async(req, res) => {
 
         enviarCorreo()
     };
-    // FUNCION QUE ENVIA AL CORREO NOTIFICACION DE SOLICITUD DE PRESTAMOS A ROSFAL
 
-    async function notificacionCorreo() {
-        try {
-            const from = "contacto@rosfal.com"
-            const toNotificacion = "jfalcon@rosfal.com"
-
-            // console.log(nombre + " en enviar correo");
-            // console.log(apellido + " en enviar correo");
-
-            // Configurar la conexión SMTP con el servidor de correo personalizado
-            let transporter = nodemailer.createTransport({
-                host: "mail.privateemail.com",
-                port: 465, // El puerto puede variar según la configuración de su servidor
-                secure: true, // Si utiliza SSL/TLS, establezca este valor en true
-                tls: {
-                    rejectUnauthorized: false
-                },
-                auth: {
-                    user: process.env.USERCORREO,
-                    pass: process.env.PASSCORREO,
-                },
-            });
-
-            // Configurar los detalles del correo electrónico
-            let info = await transporter.sendMail({
-                from: `${from} ROSFAL SOLUCIONES DE PRESTAMOS`,
-                to: `${toNotificacion}`,
-                subject: `Nueva solicitud de prestamo cliente ${nombre} ${apellido}`,
-                html: `
-                
-                <P><strong>Asunto</strong>: Confirmación de solicitud de préstamo</p><br>
-
-                <P><strong>Cliente</strong>: ${nombre} ${apellido}</p>
-            
-                <P><strong>Cedula</strong>: ${cedula}</p>
-
-                <P><strong>Monto solicitado</strong>: ${montoSolicitado}</p>
-
-                <P><strong>Direccion</strong>: ${direccion}</p>
-
-                <P><strong>Email</strong>: ${email}</p>
-
-                <P><strong>Celular</strong>: ${celular}</p><br>
-            
-                <P><strong>Atentamente,</p><br>
-            
-                <h4 style="color: #2D8DBD;">ROSFAL SOLUCIONES DE PRESTAMOS</h4>
-                <P><strong>T.</strong> 829-856-0203 <strong>EMAIL.</strong> contacto@rosfal.com </P>
-                <P>Síguenos en <strong>FB:</strong> Rosfalrd <strong>IG:</strong> @Rosfalrd </P>
-                <a href="www.rosfal.com">www.rosfal.com</a>
-                `
-
-            });
-
-            console.log("Correo enviado: %s", info.messageId);
-
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    notificacionCorreo()
 
     // ----------------------------------
 
