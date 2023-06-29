da9d9cefafdc3102e02ee4dc6823d5d7431e0245
Jose Miguel Falcon <jfalcon89@hotmail.com>
2023-06-29 15:01:20 -0400

    se corrige el ancho de los contratos para imprimir

--- routes/mantenimiento.js
+++ routes/mantenimiento.js
@@ -6,24 +6,46 @@ const pool = require("../database");
 
 
 // 1
-// RENDERIZANDO Y MOSTRANDO TODOS LOS MENSAJES NUEVOS
-router.get('/mensajes', async(req, res) => {
+// RENDERIZANDO Y MOSTRANDO TODOS LOS DATOS DE REPORTES
+router.get('/mantenimiento', async(req, res) => {
     if (req.session.loggedin) {
 
 
-        const arrayMensajesDB = await pool.query('SELECT * FROM mensajes WHERE estadoMensaje="Nuevo" ORDER BY fechaMensaje DESC');
-        const arrayMensajesLeidosDB = await pool.query('SELECT * FROM mensajes WHERE estadoMensaje="Leido" ');
-        const arrayMensajesNoLeidosDB = await pool.query('SELECT * FROM mensajes WHERE estadoMensaje="No Leido" ');
-        // const arraySolicitudesAprobadasDB = await pool.query('SELECT * FROM solicitudes WHERE estadoSolicitud="aprobada"');
-        // const arraySolicitudesDeclinadasDB = await pool.query('SELECT * FROM solicitudes WHERE estadoSolicitud="declinada"');
-        // const arraySolicitudesEnRevisionDB = await pool.query('SELECT * FROM solicitudes WHERE estadoSolicitud="En Revision"');
-        res.render("mensajes", {
-            arrayMensajes: arrayMensajesDB,
-            arrayMensajesLeidos: arrayMensajesLeidosDB,
-            arrayMensajesNoLeidos: arrayMensajesNoLeidosDB,
+
+        const arraySolicitudesDB = await pool.query('SELECT idSolicitud FROM solicitudes WHERE estadoSolicitud="nueva"');
+        const arrayMensajesNuevosDB = await pool.query('SELECT idMensaje FROM mensajes WHERE estadoMensaje="Nuevo"');
+        const arrayRutasDB = await pool.query('SELECT idRuta FROM rutas ');
+        const arrayTestimoniosNuevosDB = await pool.query('SELECT idTestimonio FROM testimonios WHERE estadoTestimonio="Nuevo" ORDER BY fechaTestimonio DESC');
+
+        const montoPrestadoDB = await pool.query("SELECT SUM(montoSolicitado) montoPrestado FROM solicitudes WHERE estadoSolicitud = 'Aprobada'")
+        const montoLiquidadoDB = await pool.query("SELECT SUM(montoSolicitado) montoLiquidado FROM solicitudes WHERE estadoSolicitud = 'Liquidado'")
+        const cantPrestamosDB = await pool.query("SELECT COUNT(idSolicitud) cantPrestamos FROM solicitudes WHERE estadoSolicitud = 'Aprobada'")
+        const prestamosAtrasosDB = await pool.query("SELECT SUM(atraso) prestamosAtrasos FROM solicitudes WHERE estadoSolicitud = 'Aprobada' AND atraso > 0")
+        const cantAtrasosDB = await pool.query("SELECT COUNT(idSolicitud) cantAtrasos FROM solicitudes WHERE estadoSolicitud = 'Aprobada' AND atraso > 0")
+
+
+        const arraySolicitudesAprobadasDB = await pool.query('SELECT * FROM solicitudes WHERE estadoSolicitud="Aprobada" ORDER BY fechaSolicitud DESC');
+        const arraySolicitudesAprobadasFirmadasDB = await pool.query('SELECT * FROM solicitudes WHERE estadoSolicitud="Aprobada" AND firmaContrato= "NO" ORDER BY fechaSolicitud DESC');
+        const arraySolicitudesAtrasadasDB = await pool.query('SELECT * FROM solicitudes WHERE estadoSolicitud="Aprobada" AND atraso > 0');
+        const arrayNotificacionAtrasoDB = await pool.query(`SELECT novedades_atrasos.idSolicitud, idNovedadAtraso, solicitudes.nombre, solicitudes.apellido, novedades_atrasos.celular, novedades_atrasos.ruta, novedades_atrasos.atraso, fechaNovedad  FROM novedades_atrasos, solicitudes WHERE novedades_atrasos.idSolicitud = solicitudes.idSolicitud ORDER BY fechaNovedad DESC`);
+        const arraySolicitudesLiquidadasDB = await pool.query('SELECT * FROM solicitudes WHERE estadoSolicitud="Liquidado"');
+        res.render("mantenimiento", {
+            arraySolicitudes: arraySolicitudesDB,
+            arrayMensajesNuevos: arrayMensajesNuevosDB,
+            arrayRutas: arrayRutasDB,
+            arraySolicitudesAprobadas: arraySolicitudesAprobadasDB,
+            arraySolicitudesAprobadasFirmadas: arraySolicitudesAprobadasFirmadasDB,
+            arraySolicitudesAtrasadas: arraySolicitudesAtrasadasDB,
+            arraySolicitudesLiquidadas: arraySolicitudesLiquidadasDB,
+            arrayTestimoniosNuevos: arrayTestimoniosNuevosDB,
+            montoPrestado: montoPrestadoDB[0].montoPrestado,
+            cantPrestamos: cantPrestamosDB[0].cantPrestamos,
+            prestamosAtrasos: prestamosAtrasosDB[0].prestamosAtrasos,
+            cantAtrasos: cantAtrasosDB[0].cantAtrasos,
+            montoLiquidado: montoLiquidadoDB[0].montoLiquidado,
+            arrayNotificacionAtraso: arrayNotificacionAtrasoDB,
             login: true,
             name: req.session.name
-
 
         });
 
@@ -35,6 +57,7 @@ router.get('/mensajes', async(req, res) => {
     }
 
 });
+
 
 
 
