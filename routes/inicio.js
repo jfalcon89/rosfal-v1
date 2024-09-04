9b559bc0eb39c9e74c6f66b8a030114971296b26
Jose Miguel Falcon <jfalcon89@hotmail.com>
2024-09-03 21:42:10 -0400

    se agrega validacion de pais y control de ataques

--- routes/inicio.js
+++ routes/inicio.js
@@ -4,36 +4,39 @@ const router = express.Router();
 const moment = require("moment");
 const pool = require("../database");
 const fetch = require("node-fetch");
-
+// const geoip = require('geoip-lite');
+const ipapi = require('ipapi.co');
 const useragent = require('express-useragent');
 
 router.get('/', async(req, res) => {
 
-    // const ipString = "148.0.27.34, 172.71.82.116";
+    // const ipString = "201.229.238.223, 172.71.82.116";
     const ipString = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const ip = ipString.split(',')[0];
-
     console.log('ip ' + ip);
+
+    // const geo = geoip.lookup(ip); // Busca la información geográfica de la IP
+    // console.log(geo)
 
     const device = req.useragent.isMobile ? 'Mobile' : 'Desktop';
     const bot = req.useragent.isBot ? 'true' : 'false';
     const browser = req.useragent.browser;
     const sistemaOperativo = req.useragent.os
     const plataforma = req.useragent.platform
+    const fecha = new Date().toLocaleString('en-US', { timeZone: 'America/Santo_Domingo' });
 
+    // const bot = 'true';
+    // const browser = 'curl';
 
-    fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=174a3d4da2a14777ab66bef79388279b&ip_address=${ip}`)
-        .then(response => response.json())
-        .then(data => {
+    if (browser !== 'python-requests' && browser !== 'Go-http-client' && browser !== 'unknown' && browser !== 'curl' && bot !== 'true') {
 
-            const pais = data.country;
-            const ciudad = data.city;
-            const latitud = data.latitude;
-            const longitud = data.longitude;
-            const fecha = new Date().toLocaleString('en-US', { timeZone: 'America/Santo_Domingo' });
+        var callback = function(res) {
+            // console.log(res);
 
-
-
+            const pais = res.country_name;
+            const ciudad = res.city;
+            const latitud = res.latitude;
+            const longitud = res.longitude;
 
             const nuevaVisita = {
                 pais,
@@ -47,40 +50,40 @@ router.get('/', async(req, res) => {
                 longitud,
                 fecha,
                 bot
-
-
             };
 
             console.log(nuevaVisita)
 
-
             pool.query('INSERT INTO visitas set ?', [nuevaVisita]);
-        })
+        };
+
+        ipapi.location(callback, `${ip}`)
 
 
-    // console.log(ip)
-    // console.log(device)
-    // console.log(browser)
+        const arrayNuevoTestimonioDB = await pool.query('SELECT * FROM testimonios WHERE estadoTestimonio="Activo" ');
+        const modal_prestamos_quincenalesDB = await pool.query("SELECT * FROM tab_anuncios WHERE idAnuncio = 1")
+        const modal_prestamos_semanalesDB = await pool.query("SELECT * FROM tab_anuncios WHERE idAnuncio = 2")
+        const modal_anuncios_generalesDB = await pool.query("SELECT * FROM tab_anuncios WHERE idAnuncio = 3")
 
-    const arrayNuevoTestimonioDB = await pool.query('SELECT * FROM testimonios WHERE estadoTestimonio="Activo" ');
+        res.render('inicio', {
+            arrayNuevoTestimonio: arrayNuevoTestimonioDB,
+            modal_prestamos_quincenales: modal_prestamos_quincenalesDB[0],
+            modal_prestamos_semanales: modal_prestamos_semanalesDB[0],
+            modal_anuncios_generales: modal_anuncios_generalesDB[0]
+        });
 
-    const modal_prestamos_quincenalesDB = await pool.query("SELECT * FROM tab_anuncios WHERE idAnuncio = 1")
-    const modal_prestamos_semanalesDB = await pool.query("SELECT * FROM tab_anuncios WHERE idAnuncio = 2")
-    const modal_anuncios_generalesDB = await pool.query("SELECT * FROM tab_anuncios WHERE idAnuncio = 3")
+    } else {
+        res.render('404', {});
 
-
-    // console.log(estadoAnuncioDB[0].estado)
-
-    res.render('inicio', {
-        arrayNuevoTestimonio: arrayNuevoTestimonioDB,
-        modal_prestamos_quincenales: modal_prestamos_quincenalesDB[0],
-        modal_prestamos_semanales: modal_prestamos_semanalesDB[0],
-        modal_anuncios_generales: modal_anuncios_generalesDB[0]
+    };
 
 
-    });
 
 });
+
+
+
+
 
 
 //INSERTAR NUEVO TESTIMONIO****************
--- routes/solicita-ya.js
+++ routes/solicita-ya.js
@@ -5,7 +5,7 @@ const moment = require("moment");
 const pool = require("../database");
 const fetch = require("node-fetch");
 const nodemailer = require("nodemailer");
-
+const ipapi = require('ipapi.co');
 const useragent = require('express-useragent');
 
 // no se esta usando
@@ -25,7 +25,7 @@ router.post("/solicita-ya", async(req, res) => {
     const { cedula, nombre, apellido, sexo, estadoCivil, direccion, direccionNegocio, tiempoNegocio, email, telefono, celular, nacionadlidad, nombreFamilia, direccionFamilia, parentescoFamilia, telefonoFamilia, apodoFamilia, empresa, salario, puesto, dirEmpresa, telefonoEmpresa, departamento, tiempoEmpresa, nombreRefPers1, nombreRefPers2, telefonoRefPer1, telefonoRefPer2, tipoPrestamo, banco, numeroCuenta, montoSolicitado, frecuenciaPagos, cantidadPagosSemanales, cantidadPagosDiarios, cantidadPagosQuincenales, cantidadPagosMensuales } = req.body;
 
     // OBTENCION DE PARAMAMETROS CLIENTE
-    // const ipString = "152.0.12.42, 172.71.82.116";
+    // const ipString = "201.229.238.223, 172.71.82.116";
     const ipString = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const ip = ipString.split(',')[0];
 
@@ -35,40 +35,42 @@ router.post("/solicita-ya", async(req, res) => {
     const plataforma = req.useragent.platform
     const fecha = new Date().toLocaleString('en-US', { timeZone: 'America/Santo_Domingo' });
 
-    fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=174a3d4da2a14777ab66bef79388279b&ip_address=${ip}`)
-        .then(response => response.json())
-        .then(data => {
+    // fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=174a3d4da2a14777ab66bef79388279b&ip_address=${ip}`)
+    //     .then(response => response.json())
+    //     .then(data => {
 
-            // FUNCION QUE ENVIA AL CORREO NOTIFICACION DE SOLICITUD DE PRESTAMOS A ROSFAL
+    var callback = function(res) {
 
-            async function notificacionCorreo() {
-                try {
-                    const from = "contacto@rosfal.com"
-                    const toNotificacion = "jfalcon@rosfal.com"
+        // FUNCION QUE ENVIA AL CORREO NOTIFICACION DE SOLICITUD DE PRESTAMOS A ROSFAL
 
-                    // console.log(nombre + " en enviar correo");
-                    // console.log(apellido + " en enviar correo");
+        async function notificacionCorreo() {
+            try {
+                const from = "contacto@rosfal.com"
+                const toNotificacion = "jfalcon@rosfal.com"
 
-                    // Configurar la conexión SMTP con el servidor de correo personalizado
-                    let transporter = nodemailer.createTransport({
-                        host: "mail.privateemail.com",
-                        port: 465, // El puerto puede variar según la configuración de su servidor
-                        secure: true, // Si utiliza SSL/TLS, establezca este valor en true
-                        tls: {
-                            rejectUnauthorized: false
-                        },
-                        auth: {
-                            user: process.env.USERCORREO,
-                            pass: process.env.PASSCORREO,
-                        },
-                    });
+                // console.log(nombre + " en enviar correo");
+                // console.log(apellido + " en enviar correo");
 
-                    // Configurar los detalles del correo electrónico
-                    let info = await transporter.sendMail({
-                        from: `${from} ROSFAL SOLUCIONES DE PRESTAMOS`,
-                        to: `${toNotificacion}`,
-                        subject: `Nueva solicitud de prestamo cliente ${nombre} ${apellido}`,
-                        html: `
+                // Configurar la conexión SMTP con el servidor de correo personalizado
+                let transporter = nodemailer.createTransport({
+                    host: "mail.privateemail.com",
+                    port: 465, // El puerto puede variar según la configuración de su servidor
+                    secure: true, // Si utiliza SSL/TLS, establezca este valor en true
+                    tls: {
+                        rejectUnauthorized: false
+                    },
+                    auth: {
+                        user: process.env.USERCORREO,
+                        pass: process.env.PASSCORREO,
+                    },
+                });
+
+                // Configurar los detalles del correo electrónico
+                let info = await transporter.sendMail({
+                    from: `${from} ROSFAL SOLUCIONES DE PRESTAMOS`,
+                    to: `${toNotificacion}`,
+                    subject: `Nueva solicitud de prestamo cliente ${nombre} ${apellido}`,
+                    html: `
             
             <P><strong>Asunto</strong>: Confirmación de solicitud de préstamo</p><br>
 
@@ -78,15 +80,15 @@ router.post("/solicita-ya", async(req, res) => {
             <P><strong>Direccion</strong>: ${direccion}</p>
             <P><strong>Email</strong>: ${email}</p>
             <P><strong>Celular</strong>: ${celular}</p>
-            <P><strong>Pais</strong>: ${data.country}</p>
-            <P><strong>Ciudad</strong>: ${data.city}</p>
+            <P><strong>Pais</strong>: ${res.country_name}</p>
+            <P><strong>Ciudad</strong>: ${res.city}</p>
             <P><strong>Direccion ip</strong>: ${ip}</p>
             <P><strong>Dispositivo</strong>: ${device}</p>
             <P><strong>Navegador</strong>: ${browser}</p>
             <P><strong>Sistema Operativo</strong>: ${sistemaOperativo}</p>
             <P><strong>Plataforma</strong>: ${plataforma}</p>
             <P><strong>Fecha</strong>: ${fecha}</p>
-            <P><strong>Latitud</strong>: ${data.latitude} <strong>longitud</strong>: ${data.longitude}</p><br>
+            <P><strong>Latitud</strong>: ${res.latitude} <strong>longitud</strong>: ${res.longitude}</p><br>
         
             <P>Atentamente,</p>
         
@@ -96,19 +98,20 @@ router.post("/solicita-ya", async(req, res) => {
             <a href="www.rosfal.com">www.rosfal.com</a>
             `
 
-                    });
+                });
 
-                    console.log("Correo enviado: %s", info.messageId);
+                console.log("Correo enviado: %s", info.messageId);
 
-                } catch (error) {
-                    console.log(error);
-                }
+            } catch (error) {
+                console.log(error);
             }
+        }
 
-            notificacionCorreo()
+        notificacionCorreo()
 
 
-        })
+    };
+    ipapi.location(callback, `${ip}`)
 
 
     // FUNCION QUE ENVIA NOTIFICACION DE SOLICITUD DE PRESTAMOS AL CLIENTE
